cb0756e7ad2f172e9f0fd02866fb01ecf5ec8ff2
BramG08 <bgrippeling08@gmail.com>
2025-02-13 12:53:28 +0100

    07 completed

---
+++ public/01/datatypes.php
@@ -0,0 +1,13 @@
+<?php 
+$huisnummer = "Roeier";
+echo $huisnummer;
+$huisnummer = 89;
+echo $huisnummer."<br>";
+
+
+$GeslaagdVoorRijbewijs = true;
+echo $GeslaagdVoorRijbewijs."<br>";
+
+$PrijsBenzine = 1.5;
+echo $PrijsBenzine."<br>";
+?>
---
+++ public/01/showtypes.php
@@ -0,0 +1,14 @@
+<?php 
+$nummer = 89;
+$salaris = 0.5;
+$zin = "hello zin";
+$waarOfNiet= false;
+
+$rijtje = [1,2,3,4,5];
+echo gettype($nummer)."<br>";
+echo gettype($salaris)."<br>";  
+echo gettype($zin)."<br>";
+echo gettype($waarOfNiet)."<br>";
+echo gettype($rijtje)."<br>";
+
+?>
