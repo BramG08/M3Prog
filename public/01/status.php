33432a6ec722185d13e2bd8f0b855eb25843c775
BramG08 <bgrippeling08@gmail.com>
2025-02-13 12:28:34 +0100

    05 completed

---
+++ public/01/status.php
@@ -0,0 +1,26 @@
+<?php 
+
+$status = "Offline available";
+echo $status."<br>";
+$status = "Online available";
+echo $status."<br>";
+$status = "Busy";
+echo $status."<br>";
+$status = "Do not disturb";
+echo $status."<br>";
+$status = "Offline";
+echo $status."<br>";
+
+$prijs = 20.5;
+echo $prijs."<br>";
+$prijs = 21.5;
+echo $prijs."<br>";
+$prijs = 30.5;
+echo $prijs."<br>";
+$prijs = 120.5;
+echo $prijs."<br>";
+$prijs = 88.99;
+echo $prijs."<br>";
+$prijs = 1.4;
+
+?>
