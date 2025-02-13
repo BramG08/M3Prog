16fd1b756a80202b2b054a273068d5171ac81824
BramG08 <bgrippeling08@gmail.com>
2025-02-13 11:53:28 +0100

    01 firstphp

---
+++ public/01/its-me.php
@@ -0,0 +1,3 @@
+<?php 
+echo "favortiete film:Cars2,favortiete game :NBA2K,klas:SD1C"
+?>
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php 
+echo "<h1>Normaal Kopje</h1>";
+echo "<p>Paragraaf</p>";
+echo "<h2>Kleiner kopje</h2>";
+echo "<h3>Kleinste kopje</h3>";
+echo "<p>Paragraaf</p>";
+?>
