bf080ed9d06b4e98c0079b25555c7b8278b4c521
BramG08 <bgrippeling08@gmail.com>
2025-02-27 13:00:14 +0100

    finished 02

---
+++ public/02/item.php
@@ -0,0 +1,23 @@
+<?php 
+$BaseName = "Normal Crossbow";
+$TwoHandDamage = "9~16";
+$StrengthRequired = "40";
+$DexterityRequired = "33";
+$Sockets = "2";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crossbow Stats</title>
+</head>
+<body>
+    <h1><?=$BaseName?></h1>
+    <p> Two-Hand Damage: <?=$TwoHandDamage?><br> </p>
+    <p> Strength Required: <?=$StrengthRequired?><br> </p>
+    <p> Dexterity Required: <?=$DexterityRequired?> <br></p>
+    <p> Sockets: <?=$Sockets?> <br></p>
+    
+</body>
+</html>
