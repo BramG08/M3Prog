ddbd77ab8dbe829e563927a44fb61066d9033a9e
BramG08 <bgrippeling08@gmail.com>
2025-02-27 12:06:51 +0100

    Created plak.php

---
+++ public/02/plak.php
@@ -0,0 +1,14 @@
+<?php 
+$standaardString = 'hello world<br>';
+$extraString = "extra world<br>";
+echo $standaardString;
+echo $extraString;
+
+
+$voorNaam = "luigi";
+$achterNaam = "mario";
+$fullName = "hello" ." ".$voorNaam. " ".$achterNaam "<br>";
+echo $fullName; 
+
+
+?>
