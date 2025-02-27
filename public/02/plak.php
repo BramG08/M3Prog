53ad497afcf03529b2a2c6a19c3cc81ebc2a4a5f
BramG08 <bgrippeling08@gmail.com>
2025-02-27 12:14:50 +0100

    Updated plak

--- public/02/plak.php
+++ public/02/plak.php
@@ -7,8 +7,24 @@ echo $extraString;
 
 $voorNaam = "luigi";
 $achterNaam = "mario";
-$fullName = "hello" ." ".$voorNaam. " ".$achterNaam "<br>";
+$fullName = "hello" ." ".$voorNaam. " ".$achterNaam ."<br>";
 echo $fullName; 
+
+$naam = "Yoshi";
+$ondertekentDoor = "directeur Bowser";
+$enemy = "Koopas";
+
+$emailTekst = "Beste $naam<br>
+Helaas moeten wij uw verzoek afwijzen.<br>
+Onze $enemy vinden het niet leuk om opgegeten te worden<br>
+<br>
+Hoogachtend<br>
+$ondertekentDoor<br>
+<br>
+PS:<br>
+De princess is in haar kasteel... voor nu :>";
+echo $emailTekst;
+
 
 
 ?>
