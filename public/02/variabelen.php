d7b74e25eec64f79bb499459f9bff0589e2fa79e
BramG08 <bgrippeling08@gmail.com>
2025-02-27 12:44:53 +0100

    variabelen finished

---
+++ public/02/variabelen.php
@@ -0,0 +1,17 @@
+<?php      
+$stad = "New York City";
+$weer = "zonnig";
+$aantalMensenInStad = 10000000;
+$temperatuur = 21.5;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?="In $stad leven $aantalMensenInStad mensen. Het is $weer de temperatuur is nu $temperatuur graden."?></p>
+</body>
+</html>
