c13f1a6042940e9dafa36a127b7d77b23275246a
BramG08 <bgrippeling08@gmail.com>
2025-03-10 12:48:31 +0100

    Update phpfunctions.php

--- public/05/phpfunctions.php
+++ public/05/phpfunctions.php
@@ -18,4 +18,31 @@ print($woordLengte2);
 //vraag 2.5: hij returned een int
 //vraag 3.5: een interger
 //vraag 4: Ik heb de strlen function 2 keer aangeroepen
+
+
+$tweeMachtTwee = pow(2,2);
+print (pow(2,2));
+echo "<br>";
+
+$tweeMAchtTien = pow(2, 10);
+print (pow(2, 10));
+echo  "<br>";
+
+$derdeMacht = pow(3,4);
+print("<br>");
+print(pow(3,4));
+
+print("<br>");
+
+// vraag 1) We geven het resultaat door met een print functie
+// strtoupper bron: https://www.php.net/manual/en/function.strtoupper.php
+
+    $naam = "Wario";
+    $naamGroot = strtoupper($naam);
+    print($naamGroot);
+
+    print("<br>");
+    $naamKlein = strtolower($naam);
+    print($naamKlein)
+
 ?>
