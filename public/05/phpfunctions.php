9961eafbf2b048e6ea879f771d603214010f6120
BramG08 <bgrippeling08@gmail.com>
2025-03-10 12:20:53 +0100

    function 01-01 finished

---
+++ public/05/phpfunctions.php
@@ -0,0 +1,21 @@
+<?php
+
+$hoeLangIsDitWoord = "scrabbleWoord";
+
+$woordLengte = strlen($hoeLangIsDitWoord);
+print($woordLengte);
+
+//vraag 1: het type is een string
+//vraag 2: hij returned een int
+//vraag 3: een interger 
+
+$hoeLangIsDitWoord2 = "octopussenslikken";
+
+$woordLengte2 = strlen($hoeLangIsDitWoord2);
+print($woordLengte2);
+
+//vraag 1.5: Het type is een string 
+//vraag 2.5: hij returned een int
+//vraag 3.5: een interger
+//vraag 4: Ik heb de strlen function 2 keer aangeroepen
+?>
