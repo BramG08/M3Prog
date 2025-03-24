4013f4461ab98dc91285a93e0146a3b89ed92cf9
BramG08 <bgrippeling08@gmail.com>
2025-03-24 12:53:28 +0100

    https codes

---
+++ public/06/hdr302.php
@@ -0,0 +1,7 @@
+<?php 
+
+header('Location: https://www.youtube.com/watch?v=GMgsFZ4rkEI');
+exit;
+
+
+?>
---
+++ public/06/hdr404.php
@@ -0,0 +1,11 @@
+<?php 
+
+header( 'Status: 404 Not Found' );
+echo "<h1>404 Not Found</h1>";
+
+
+exit;
+
+
+
+?>
