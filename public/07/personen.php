36e2f538c7462cdceaef505f89ee83e30c900e2f
BramG08 <bgrippeling08@gmail.com>
2025-03-31 12:08:04 +0200

    Create  personen.php

---
+++ public/07/personen.php
@@ -0,0 +1,15 @@
+<?php 
+$persoon = ['voornaam'=> 'Luigi', 'achternaam' => 'Mario', 'leeftijd' => 19,'woonplaats' => 'mushroomkingdom', 'favo eten' => 'pasta', 'minst favo eten' => 'paddestoelen'];
+
+echo "<table>";
+
+echo "<th>key</th><th>value</th>";
+foreach($persoon as $key => $value)
+{
+    echo "<tr><td>$key</td><td>$value</td></tr>";
+};
+
+echo "</table>";
+
+
+?>
